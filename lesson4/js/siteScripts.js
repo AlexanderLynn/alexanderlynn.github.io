f9861fce98d2d8522539bb5bdcf0267d31126b1c
AlexanderLynn <lynnalexander98@outlook.com>
2021-02-02 14:11:31 -0700

    Time fixed

--- lesson4/js/siteScripts.js
+++ lesson4/js/siteScripts.js
@@ -1,6 +1,9 @@
 function getTime() {
-    document.getElementById("time").innerHTML = new Date().getDay();
-    document.getElementById("time").innerHTML += " " + new Date().getMonth();
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("time").innerHTML = days[new Date().getDay()] + ",";
+    document.getElementById("time").innerHTML += " " + new Date().getDay();
+    document.getElementById("time").innerHTML += " " + months[new Date().getMonth()];
     document.getElementById("time").innerHTML += " " + new Date().getFullYear();
 }
 function toggleMenu() {
