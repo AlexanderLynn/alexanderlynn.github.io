dd1b60191694b0e025869806571a9322efc1b745
AlexanderLynn <lynnalexander98@outlook.com>
2021-02-02 17:30:07 -0700

    Ready for Main Content

---
+++ lesson5/js/siteScripts.js
@@ -0,0 +1,21 @@
+function getTime() {
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("time").innerHTML = days[new Date().getDay()] + ",";
+    document.getElementById("time").innerHTML += " " + new Date().getDay();
+    document.getElementById("time").innerHTML += " " + months[new Date().getMonth()];
+    document.getElementById("time").innerHTML += " " + new Date().getFullYear();
+}
+//Banner Code. This way you can set whatever value you want in the JS.
+window.addEventListener('load', (event) => {
+    if (new Date().getDay() == 4)
+    {
+        document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    }
+    else
+        document.getElementById("banner").style.display = "none";
+});
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("hide");
+}
