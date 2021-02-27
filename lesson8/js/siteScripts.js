895667d302fc8bfe2daf1b8810f6f65682982088
AlexanderLynn <lynnalexander98@outlook.com>
2021-02-27 14:53:43 -0700

    weight bug fix

--- lesson8/js/siteScripts.js
+++ lesson8/js/siteScripts.js
@@ -13,15 +13,6 @@ window.addEventListener('load', (event) => {
     let windChill = 35.74 + 0.6215 * temp - 35.75 * (Math.pow(speed, 0.16)) + 0.4275 * temp * (Math.pow(speed, 0.16));
     document.getElementById("windChill").innerHTML = windChill.toFixed(1);
 });
-//Banner Code. This way you can set whatever value you want in the JS.
-window.addEventListener('load', (event) => {
-    if (new Date().getDay() == 5)
-    {
-        document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    }
-    else
-        document.getElementById("banner").style.display = "none";
-});
 
 window.addEventListener('load', (event) => {
     WebFont.load({ google: {families: ['Architects Daughter', 'Raleway']}});
