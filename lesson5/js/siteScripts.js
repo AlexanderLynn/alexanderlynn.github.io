bb242e2b37094d429e2500cb8b79c08ac5798436
AlexanderLynn <lynnalexander98@outlook.com>
2021-02-27 14:46:32 -0700

    JS fix and Stormcenter initial commit

--- lesson5/js/siteScripts.js
+++ lesson5/js/siteScripts.js
@@ -2,7 +2,7 @@ function getTime() {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     document.getElementById("time").innerHTML = days[new Date().getDay()] + ",";
-    document.getElementById("time").innerHTML += " " + new Date().getDay();
+    document.getElementById("time").innerHTML += " " + new Date().getDate();
     document.getElementById("time").innerHTML += " " + months[new Date().getMonth()];
     document.getElementById("time").innerHTML += " " + new Date().getFullYear();
 }
