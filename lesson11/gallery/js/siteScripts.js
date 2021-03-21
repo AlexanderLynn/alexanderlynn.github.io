faab0658d503c8b034173947a3eb3fd0a3772de4
AlexanderLynn <lynnalexander98@outlook.com>
2021-03-20 22:43:52 -0600

    javascript error fix

--- lesson11/gallery/js/siteScripts.js
+++ lesson11/gallery/js/siteScripts.js
@@ -6,13 +6,7 @@ function getTime() {
     document.getElementById("time").innerHTML += " " + months[new Date().getMonth()];
     document.getElementById("time").innerHTML += " " + new Date().getFullYear();
 }
-//Wind Chill Code
-window.addEventListener('load', (event) => {
-    let temp = parseInt(document.getElementById("temp").innerHTML);
-    let speed = parseInt(document.getElementById("windSpeed").innerHTML);
-    let windChill = 35.74 + 0.6215 * temp - 35.75 * (Math.pow(speed, 0.16)) + 0.4275 * temp * (Math.pow(speed, 0.16));
-    document.getElementById("windChill").innerHTML = windChill.toFixed(1);
-});
+
 //Banner Code. This way you can set whatever value you want in the JS.
 window.addEventListener('load', (event) => {
     if (new Date().getDay() == 5)
--- lesson11/storm_center/js/siteScripts.js
+++ lesson11/storm_center/js/siteScripts.js
@@ -6,14 +6,6 @@ function getTime() {
     document.getElementById("time").innerHTML += " " + months[new Date().getMonth()];
     document.getElementById("time").innerHTML += " " + new Date().getFullYear();
 }
-//Wind Chill Code
-window.addEventListener('load', (event) => {
-    let temp = parseInt(document.getElementById("temp").innerHTML);
-    let speed = parseInt(document.getElementById("windSpeed").innerHTML);
-    let windChill = 35.74 + 0.6215 * temp - 35.75 * (Math.pow(speed, 0.16)) + 0.4275 * temp * (Math.pow(speed, 0.16));
-    document.getElementById("windChill").innerHTML = windChill.toFixed(1);
-});
-
 window.addEventListener('load', (event) => {
     WebFont.load({ google: {families: ['Architects Daughter', 'Raleway']}});
 });
